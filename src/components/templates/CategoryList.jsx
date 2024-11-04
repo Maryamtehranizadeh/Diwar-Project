5d744fd3bd4bec276f10c0953975d0f5f479df54
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-11-04 23:16:00 +0000

    some styles

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -2,6 +2,7 @@ import { getCategory } from "../../services/admin";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import Loader from "../modules/Loader";
 import { deleteCategory } from "../../services/admin";
+import styles from "./CategoryList.module.css";
 
 function CategoryList() {
   const { isLoading, data } = useQuery(["get-categories"], getCategory);
@@ -17,16 +18,14 @@ function CategoryList() {
     deleteMutation.mutate(id);
   };
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
         data.data.map((i) => (
-          <div key={i._id}>
-            <p>
-              <img src={`${i.icon}.svg`} />
-            </p>
-            <h4>{i.name}</h4>
+          <div key={i._id} className={styles.category}>
+            <img src={`${i.icon}.svg`} />
+            <h5>{i.name}</h5>
             <p>{i.slug}</p>
             <button onClick={() => deleteHandler(i._id)}>
               Delete Category
